26db35c6f1f5da695a231942cba4457b8285ac8d
unknown <jangjaeko45@gmail.com>
2026-01-19 18:28:50 -0800

    feat: create review with server action

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from "next/navigation";
 import style from "./page.module.css";
+import { createReviewAction } from "@/actions/create-review.action";
 
 // export const dynamicParams = false;
 // if true, generateStaticParams can generate only some of the paths
@@ -9,14 +10,9 @@ export function generateStaticParams() {
   return [{ id: "1" }, { id: "2" }, { id: "3" }]; // only use string values
 }
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ id: string | string[] }>;
-}) {
-  const { id } = await params;
+async function Detail({ id }: { id: string }) {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${id}`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${id}`,
   );
   if (!response.ok) {
     if (response.status === 404) {
@@ -26,9 +22,8 @@ export default async function Page({
   }
   const book = await response.json();
   const { title, subTitle, description, author, publisher, coverImgUrl } = book;
-
   return (
-    <div className={style.container}>
+    <section>
       <div
         className={style.cover_img_container}
         style={{ backgroundImage: `url('${coverImgUrl}')` }}
@@ -41,6 +36,55 @@ export default async function Page({
         {author} | {publisher}
       </div>
       <div className={style.description}>{description}</div>
+    </section>
+  );
+}
+
+function ReviewEditor({ id }: { id?: string }) {
+  // async function createReviewAction(formData: FormData) {
+  //   "use server";
+  //   const content = formData.get("content")?.toString();
+  //   const author = formData.get("author")?.toString(); //author: FormDataEntryValue | null => string or file or null
+
+  //   if (!content || !author) {
+  //     return;
+  //   }
+  //   try {
+  //     const response = await fetch(
+  //       `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
+  //       {
+  //         method: "POST",
+  //         body: JSON.stringify({ bookId: id, content, author }),
+  //       },
+  //     );
+  //   } catch (error) {
+  //     console.error("Failed to create review:", error);
+  //   }
+  // }
+
+  return (
+    <section>
+      <form action={createReviewAction}>
+        <input name="bookId" value={id} hidden readOnly />
+        <input required name="content" placeholder="review content" />
+        <input required name="author" placeholder="author" />
+        <button type="submit">Submit Review</button>
+      </form>
+    </section>
+  );
+}
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  return (
+    <div className={style.container}>
+      <Detail id={id} />
+      <ReviewEditor id={id} />
     </div>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,19 +3,19 @@ import Link from "next/link";
 import style from "./layout.module.css";
 import { BookData } from "@/types";
 
-// // request book api multple times in other pages but next call only one time because of request memoization
-// async function Footer() {
-//   const response = await fetch(
-//     process.env.NEXT_PUBLIC_API_SERVER_URL + "/book",
-//     // { cache: "force-cache" } // prevent all pages not to be dynamic because of this fetch
-//   );
-//   if (!response.ok) {
-//     return <div>failed to load book count</div>;
-//   }
-//   const books: BookData[] = await response.json();
-//   const bookCount = books.length;
-//   return <footer>@jaeho - total book count: {bookCount}</footer>;
-// }
+// request book api multple times in other pages but next call only one time because of request memoization
+async function Footer() {
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_API_SERVER_URL + "/book",
+    { cache: "force-cache" }, // prevent all pages not to be dynamic because of this fetch
+  );
+  if (!response.ok) {
+    return <div>failed to load book count</div>;
+  }
+  const books: BookData[] = await response.json();
+  const bookCount = books.length;
+  return <footer>@jaeho - total book count: {bookCount}</footer>;
+}
 
 export default function RootLayout({
   children,
@@ -30,7 +30,7 @@ export default function RootLayout({
             <Link href={"/"}>📚BOOK STORE</Link>
           </header>
           <main>{children}</main>
-          {/* <Footer /> */}
+          <Footer />
         </div>
       </body>
     </html>
