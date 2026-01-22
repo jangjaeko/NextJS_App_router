9679557ad57b190b48d676a7f7ecdca57ab42816
unknown <jangjaeko45@gmail.com>
2026-01-21 16:13:55 -0800

    feat : parallel ui example

---
+++ src/app/parallel/default.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Default() {
+  // for children routes refresh, to prevent pop out 404 page
+  return (
+    <div>
+      <>par default</>
+    </div>
+  );
+}
---
+++ src/app/parallel/layout.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function Layout({
+  children,
+  sidebar,
+  feed,
+}: {
+  children: React.ReactNode;
+  sidebar: React.ReactNode;
+  feed: React.ReactNode;
+}) {
+  return (
+    <div>
+      <div>
+        <Link href="/parallel">Parallel Home</Link> |{" "}
+        <Link href="/parallel/setting">Setting</Link>
+      </div>
+      <br />
+      {sidebar}
+      {feed}
+      {children}
+    </div>
+  );
+}
